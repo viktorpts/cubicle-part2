ac5775c3b86e8738fb4e020add21476fc9baf0ea
Viktor Kostadinov <viktor.pts@abv.bg>
2021-06-03 21:13:57 +0300

    Added comments

--- services/storage.js
+++ services/storage.js
@@ -1,4 +1,5 @@
 const Cube = require('../models/Cube');
+const Comment = require('../models/Comment');
 
 /* Model structure
 
@@ -16,7 +17,8 @@ async function init() {
             getAll,
             getById,
             create,
-            edit
+            edit,
+            createComment
         };
         next();
     };
@@ -44,7 +46,7 @@ async function getAll(query) {
 }
 
 async function getById(id) {
-    const cube = await Cube.findById(id).lean();
+    const cube = await Cube.findById(id).populate('comments').lean();
     if (cube) {
         return cube;
     } else {
@@ -68,9 +70,24 @@ async function edit(id, cube) {
     return existing.save();
 }
 
+async function createComment(cubeId, comment) {
+    const cube = await Cube.findById(cubeId);
+
+    if (!cube) {
+        throw new ReferenceError('No such ID in database');
+    }
+
+    const newComment = new Comment(comment);
+    await newComment.save();
+
+    cube.comments.push(newComment);
+    await cube.save();
+}
+
 module.exports = {
     init,
     getAll,
     getById,
-    create
+    create,
+    createComment
 };
