e1f398ba0b7e468bc0955433acee16928df2725c
Viktor Kostadinov <viktor.pts@abv.bg>
2021-06-03 19:39:11 +0300

    Added mongodb connection

---
+++ models/Cube.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+
+const schema = new Schema({
+    name: String,
+    description: String,
+    imageUrl: String,
+    difficulty: Number
+});
+
+module.exports = model('Cube', schema);
